675d1bf6fa43cf4ccd1f773bf1693bc471a4a743
Quied <quiedcpp@gmail.com>
2022-10-08 10:57:47 +0300

    Sock

---
+++ Client.cpp
@@ -0,0 +1,57 @@
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+
+
+using tcp = boost::asio::ip::tcp;
+
+
+std::string GetData(tcp::socket& socket) {
+
+	boost::asio::streambuf Buffer;
+
+	boost::asio::read_until(socket, Buffer, "\n");
+	std::string data = boost::asio::buffer_cast<const char*>(Buffer.data());
+	return data;
+
+}
+
+void SendData(tcp::socket& socket, const std::string& msg) {
+
+	boost::asio::write(socket, boost::asio::buffer(msg + "\n"));
+
+}
+
+int main(char* argv[], int argc) {
+
+	boost::asio::io_service service;
+
+	tcp::socket socket(service);
+
+	socket.connect(tcp::endpoint(
+		boost::asio::ip::address::from_string("127.0.0.1"), 9999));
+
+	std::cout << "Name: ";
+	std::string name, reply, response;
+	getline(std::cin, name);
+
+	// Send name
+	SendData(socket, name);
+
+	while(true){
+	
+		response = GetData(socket);
+		response.pop_back();
+
+		if (response == "exit") break;
+		std::cout << "Server: " << response << std::endl;
+
+		std::cout << name << ": ";
+		getline(std::cin, reply);
+		SendData(socket, reply);
+
+		if (reply == "exit") break;
+
+	}
+	return 0;
+}
---
+++ Source.cpp
@@ -0,0 +1,59 @@
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+
+
+using tcp = boost::asio::ip::tcp;
+
+std::string GetData(tcp::socket &socket) {
+
+	boost::asio::streambuf Buffer;
+
+	boost::asio::read_until(socket, Buffer, "\n");
+	std::string data = boost::asio::buffer_cast<const char*>(Buffer.data());
+	return data;
+
+}
+
+void SendData(tcp::socket &socket, const std::string &msg) {
+
+	boost::asio::write(socket, boost::asio::buffer(msg + "\n"));
+
+}
+
+
+int main(int argc, char* arcv[]) {
+
+	boost::asio::io_service service;
+
+	// Listening new connection
+	tcp::acceptor acceptor(service, tcp::endpoint(tcp::v4(), 9999));
+
+	tcp::socket socket(service);
+
+	// waiting on connection
+	acceptor.accept(socket);
+
+	std::string name = GetData(socket);
+	name.pop_back();
+
+	std::string response, reply;
+	reply = "Hello " + name + "!";
+	std::cout << "Server: " << reply << std::endl;
+	SendData(socket, reply);
+
+	while (true) {
+		response = GetData(socket);
+		response.pop_back();
+
+		if (response == "exit") break;
+
+		std::cout << name << ": " << response;
+
+		std::cout << "Server: ";  std::getline(std::cin, reply);
+		SendData(socket, reply);
+		if (reply == "exit") break;
+	}
+
+	return 0;
+}
